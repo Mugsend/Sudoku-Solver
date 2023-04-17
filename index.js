ea6241976ab1754d49470d296cad4b5757dc5268
Mugsend <chaudharysaurabh93063@gmail.com>
2023-04-17 23:03:08 +0530

    hola hola hee

--- index.js
+++ index.js
@@ -9,23 +9,36 @@ const inputGrid = [
   [4, 0, 0, 0, 9, 0, 7, 0, 1],
   [0, 0, 0, 0, 0, 4, 2, 0, 0],
 ];
+var wrong = false;
+var select = -1;
+const notes = getNotes(inputGrid);
+function copyTwoDimensionalArray(arr) {
+  const newArray = [];
+  for (let i = 0; i < arr.length; i++) {
+    newArray[i] = arr[i].slice();
+  }
+  return newArray;
+}
 
-const notes = getNotes();
-
-var filled = 0;
+var inputFilled = 0;
 (function initiateGrid() {
-  let grid = document.createElement("div");
+  const grid = document.createElement("div");
   grid.id = "grid";
   for (let i = 0; i < 9; i++) {
     for (let j = 0; j < 9; j++) {
-      let cell = document.createElement("div");
+      const cell = document.createElement("div");
       cell.id = i * 9 + j;
-      cell.onclick = function () {
-        select = this.id;
-      };
+
       if (inputGrid[i][j]) {
-        filled++;
+        inputFilled++;
         cell.innerText = inputGrid[i][j];
+        cell.className = "filled";
+      } else {
+        cell.className = "empty";
+        cell.innerHTML = notes[i][j].join(" ");
+        cell.onclick = function () {
+          select = this.id;
+        };
       }
       grid.appendChild(cell);
     }
@@ -33,65 +46,117 @@ var filled = 0;
   document.body.appendChild(grid);
 })();
 
-function getNotes() {
+const filledGrid = getFilledGrid(inputGrid, inputFilled);
+
+document.addEventListener("keydown", function (event) {
+  if (event.key >= "0" && event.key <= "9") {
+    let n = parseInt(event.key);
+    if (select != -1) {
+      const i = Math.floor(select / 9);
+      const j = select % 9;
+
+      if (n) {
+        document.getElementById(select).innerText = n;
+        num = filledGrid[i][j];
+        if (n == num) {
+          inputGrid[i][j] = n;
+          document.getElementById(select).className = "filled correct";
+          document.getElementById(select).onclick = "";
+          notes[i][j] = [];
+          updateNotes(n, i, j, notes);
+          updateGrid(i, j);
+        } else {
+          wrong = true;
+          document.getElementById(select).className = "filled incorrect";
+        }
+      } else {
+        document.getElementById(select).innerText = notes[i][j].join(" ");
+        document.getElementById(select).className = "empty";
+      }
+      select = -1;
+    }
+  }
+});
+
+function updateGrid(r, c) {
+  for (let i = 0; i < 9; i++) {
+    if (!inputGrid[i][c])
+      document.getElementById(i * 9 + c).innerText = notes[i][c].join(" ");
+    if (!inputGrid[r][i])
+      document.getElementById(r * 9 + i).innerText = notes[r][i].join(" ");
+    let br = r - (r % 3) + Math.floor(i / 3);
+    let bc = c - (c % 3) + (i % 3);
+    if (!inputGrid[br][bc])
+      document.getElementById(br * 9 + bc).innerText = notes[br][bc].join(" ");
+  }
+}
+
+function getNotes(grid) {
   let notes = [];
   for (let r = 0; r < 9; r++) {
     let rowNotes = [];
     for (let c = 0; c < 9; c++) {
-      if (!inputGrid[r][c]) rowNotes.push(getCellNotes(r, c));
-      else rowNotes.push([]);
+      if (!grid[r][c]) {
+        const cellNotes = getCellNotes(r, c, grid);
+        rowNotes.push(cellNotes);
+      } else {
+        rowNotes.push([]);
+      }
     }
     notes.push(rowNotes);
   }
   return notes;
 }
 
-function getCellNotes(r, c) {
+function getCellNotes(r, c, grid) {
   let nums = [];
   for (let num = 1; num <= 9; num++) {
-    if (checkRow(num, r) && checkCol(num, c) && checkBlock(num, r, c)) {
+    if (
+      checkRow(num, r, grid) &&
+      checkCol(num, c, grid) &&
+      checkBlock(num, r, c, grid)
+    ) {
       nums.push(num);
     }
   }
   return nums;
 }
 
-function checkRow(num, r) {
+function checkRow(num, r, grid) {
   for (let i = 0; i < 9; i++) {
-    if (num == inputGrid[r][i]) return false;
+    if (num == grid[r][i]) return false;
   }
   return true;
 }
 
-function checkCol(num, c) {
+function checkCol(num, c, grid) {
   for (let i = 0; i < 9; i++) {
-    if (num == inputGrid[i][c]) return false;
+    if (num == grid[i][c]) return false;
   }
   return true;
 }
 
-function checkBlock(num, r, c) {
+function checkBlock(num, r, c, grid) {
   for (let i = 0; i < 9; i++) {
     let br = r - (r % 3) + Math.floor(i / 3);
     let bc = c - (c % 3) + (i % 3);
-    if (inputGrid[br][bc] == num) return false;
+    if (grid[br][bc] == num) return false;
   }
   return true;
 }
 
-function fill(num, r, c) {
-  updateNotes(num, r, c);
-  document.getElementById(r * 9 + c).innerHTML = num;
-  inputGrid[r][c] = num;
-  filled++;
+function fill(num, r, c, notes, grid) {
+  updateNotes(num, r, c, notes);
+  grid[r][c] = num;
 }
 
-function obviousSingles() {
+function obviousSingles(notes, grid) {
   for (let r = 0; r < 9; r++) {
     for (let c = 0; c < 9; c++) {
       if (notes[r][c].length == 1) {
         let num = notes[r][c].pop();
-        fill(num, r, c);
+
+        fill(num, r, c, notes, grid);
         return true;
       }
     }
@@ -99,33 +164,38 @@ function obviousSingles() {
   return false;
 }
 
-function updateNotes(num, r, c) {
-  console.log(num, r, c);
-  updateRow(num, r);
-  updateCol(num, c);
-  updateBlock(num, r, c);
+function updateNotes(num, r, c, notes) {
+  updateRow(num, r, notes);
+  updateCol(num, c, notes);
+  updateBlock(num, r, c, notes);
 }
 
-function updateRow(num, r) {
+function updateRow(num, r, notes) {
   for (let i = 0; i < 9; i++) {
     let ind = notes[r][i].indexOf(num);
-    if (ind != -1) notes[r][i].splice(ind, 1);
+    if (ind != -1) {
+      notes[r][i].splice(ind, 1);
+    }
   }
 }
 
-function updateCol(num, c) {
+function updateCol(num, c, notes) {
   for (let i = 0; i < 9; i++) {
     let ind = notes[i][c].indexOf(num);
-    if (ind != -1) notes[i][c].splice(ind, 1);
+    if (ind != -1) {
+      notes[i][c].splice(ind, 1);
+    }
   }
 }
 
-function updateBlock(num, r, c) {
+function updateBlock(num, r, c, notes) {
   for (let i = 0; i < 9; i++) {
     let br = r - (r % 3) + Math.floor(i / 3);
     let bc = c - (c % 3) + (i % 3);
     let ind = notes[br][bc].indexOf(num);
-    if (ind != -1) notes[br][bc].splice(ind, 1);
+    if (ind != -1) {
+      notes[br][bc].splice(ind, 1);
+    }
   }
 }
 
@@ -133,7 +203,7 @@ function rowColtoBlock(r, c) {
   return Math.floor(r / 3) * 3 + Math.floor(c / 3);
 }
 
-function getNotesFreq() {
+function getNotesFreq(notes) {
   let notesFreq = {
     row: [],
     col: [],
@@ -158,8 +228,8 @@ function getNotesFreq() {
   return notesFreq;
 }
 
-function hiddenSingles() {
-  let notesFreq = getNotesFreq();
+function hiddenSingles(notes, grid) {
+  const notesFreq = getNotesFreq(notes);
   for (let r = 0; r < 9; r++) {
     for (let c = 0; c < 9; c++) {
       for (let num of notes[r][c]) {
@@ -170,7 +240,7 @@ function hiddenSingles() {
             1
         ) {
           notes[r][c] = [];
-          fill(num, r, c);
+          fill(num, r, c, notes, grid);
           return true;
         }
       }
@@ -179,7 +249,7 @@ function hiddenSingles() {
   return false;
 }
 
-function printNotes() {
+function printNotes(notes) {
   let r = 0;
   for (let i of notes) {
     let s = "";
@@ -195,8 +265,13 @@ function printNotes() {
   }
 }
 
-function solve() {
+function getFilledGrid(inputGrid, inputfilled) {
+  const grid = copyTwoDimensionalArray(inputGrid);
+  const notes = getNotes(grid);
+  var filled = inputfilled;
   while (filled != 81) {
-    if (!obviousSingles()) hiddenSingles();
+    if (!obviousSingles(notes, grid)) hiddenSingles(notes, grid);
+    filled++;
   }
+  return grid;
 }
